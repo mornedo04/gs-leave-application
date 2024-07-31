dbd2384e5b917640d73670860603106abd441668
mornedo04 <morendo04@gmail.com>
2024-07-31 10:24:27 +0400

    remarks not requried

--- src/App.js
+++ src/App.js
@@ -302,7 +302,6 @@ function App() {
                   id="website-admin"
                   className="block col-span-1 md:col-span-4 w-full min-w-0 flex-1 rounded-none rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                   placeholder=""
-                  required
                 />
               </div>
             </section>
